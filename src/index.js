b6ed523de9776dcf1a32fa2fa0c64324972c3f1f
renato3x <pereirarenato21@gmail.com>
2024-11-01 21:38:28 -0300

    add: docker compose

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import app from './app';
 
-app.listen({ port: 3000 });
+app.listen({
+  host: process.env.SERVER_HOST || 'localhost',
+  port: 3000,
+});
